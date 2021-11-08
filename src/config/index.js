63418f35d6185d0ec2a118fed963d64753599466
jacobitkashala <jacobitkashala@gmail.com>
2021-11-08 16:25:53 +0100

    some changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 // cloud
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development1') {
   console.log('dev');
   module.exports = {
     PORT: process.env.PORTLOCAL,
