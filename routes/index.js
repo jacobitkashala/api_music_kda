1f4b60502a613a9ca4596f74a35ac1b8b8e6482e
jacobitkashala <jacobitkashala@gmail.com>
2021-12-29 05:04:06 -0500

    refactor:route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const { welcom, postSongs, getSongs, putSong, deleteSong } = require('../controllers');
-const { songPostMiddleware  } = require('../middleware');
+const { welcom } = require('../controllers');
+// const { songPostMiddleware  } = require('../middleware');postSongs, getSongs, putSong, deleteSong
 
 function routes(app) {
   app.route('/').get(welcom);
 
-  app.route('/api/musik/song').post(songPostMiddleware, postSongs).get(getSongs);
+  // app.route('/api/musik/song').post(postSongs).get(getSongs);
 
-  app.route('/api/musik/song/:uuid').get(putSong).delete(deleteSong);
+  // app.route('/api/musik/song/:uuid').get(putSong).delete(deleteSong);
 }
 module.exports = routes;
--- routes/song.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { postSongs } = require('../controllers');
-
-const {songPostMiddleware}= require('../middleware/song/post');
-
-router.post('/api/musik/song',songPostMiddleware,postSongs);
