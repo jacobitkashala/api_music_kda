68855a3228b645543d091ce4500b49f7b3a9a17f
jacobitkashala <jacobitkashala@gmail.com>
2022-03-12 15:39:27 -0500

    refactor:message welcom

--- controllers/welcom/index.js
+++ controllers/welcom/index.js
@@ -1,4 +1,4 @@
 const welcom = (req, res) => {
-  return res.status(200).send({ message: 'bienvenue API musik 😃 ' });
+  return res.status(200).send({ message: 'bienvenue API musik 😃 🎵🎼 🎤 🎧 🎷 🎸 🎹 ' });
 };
 module.exports = welcom;
