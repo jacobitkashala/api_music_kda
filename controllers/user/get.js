759d21832b4e9d740898aaed3dfc7b95ac4b1124
jacobitkashala <jacobitkashala@gmail.com>
2022-04-12 17:14:36 -0400

    refa

--- controllers/user/get.js
+++ controllers/user/get.js
@@ -2,9 +2,25 @@ const { Users } = require('../../models');
 
 const getUser = async (req, res) => {
   try {
+    const responseUser = { id: '', idUser: '', nameUser: '',sex: '',mailUser: '', roleUser: '', numberPhoneUser: '' };
+    const response = [];
     const user = await Users.findAll();
+    console.log(user)
+    user.forEach(user=>{
 
-    return res.status(200).send(user);
+      responseUser.id=user.name_user;
+      responseUser.sex=user.sex_user;
+      responseUser.idUser=user.id_user;
+      responseUser.nameUser=user.name_user;
+      responseUser.roleUser=user.role_user;
+      responseUser.mailUser=user.email_user;
+      responseUser.numberPhoneUser=user.telephone_user;
+
+      response.push(responseUser);
+      
+    })
+
+    return res.status(200).send(response);
   } catch (error) {
     return res.status(500).send({ erreur: error });
   }
