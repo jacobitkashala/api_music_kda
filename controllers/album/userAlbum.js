e5c4d7c1fcf32f9fd3ebafef3cb9aa000dfec758
jacobitkashala <jacobitkashala@gmail.com>
2022-04-21 18:11:46 -0400

    refa

--- controllers/album/userAlbum.js
+++ controllers/album/userAlbum.js
@@ -5,14 +5,14 @@ const getUserAlbum = async (req, res) => {
   try {
     const listeUserAssociatAlbm = await sequelize.query(
       `
-        SELECT name_user,role_user,email_user,sex_user,telephone_user,title_album,url_image,is_top,author
-             FROM Albums AS alb
-            JOIN Users AS usr ON alb.id_user = usr.id
+        SELECT name_user as name, id_album as id,title_album as titleAlbum, url_image as urlImage,is_top as isTop,contente_type as contenteType
+        FROM Albums AS alb
+        JOIN Users AS usr ON alb.id_user = usr.id
             ;
         `,
       { type: QueryTypes.SELECT }
     );
-
+    //
     return res.status(200).send(listeUserAssociatAlbm);
   } catch (error) {
     return res.status(500).send({ erreur: error });
