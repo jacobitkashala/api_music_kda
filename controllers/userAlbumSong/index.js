8ab7e1286eb11606a989dbbe18c480b6e423cb0d
jacobitkashala <jacobitkashala@gmail.com>
2022-04-27 19:53:53 -0400

    refactor route useralbum

--- controllers/userAlbumSong/index.js
+++ controllers/userAlbumSong/index.js
@@ -7,13 +7,13 @@ const getUserAlbum = async (req, res) => {
   //     FROM Albums AS alb
   //     JOIN  Users AS usr ON alb.id_user = usr.id
   //     ) AS UsrAlg
-  // LEFT  JOIN Songs AS sgs ON sgs.id_album = UsrAlg.id   ;
+  // LEFT  JOIN Songs AS sgs ON sgs.id_album = UsrAlg.id   ; SELECT s.*, a.title_album FROM `Songs` s JOIN Albums a ON a.id = s.id_album
   try {
     const listeUserAssociatAlbm = await sequelize.query(
-      `SELECT usr.name_user as name, alb.id as id,title_album as titleAlbum, url_image as urlImage,is_top as isTop,contente_type as contenteType
-          FROM Albums AS alb
-          JOIN  Users AS usr ON alb.id_user = usr.id 
-          ;
+      `SELECT Users.name_user,Albums.title_album,Albums.url_image,Songs.url_song,Songs.title_songs 
+      FROM Users 
+      JOIN Albums ON Albums.id=Users.id 
+      JOIN Songs ON Songs.id=Albums.id;
         `,
       { type: QueryTypes.SELECT }
     );
