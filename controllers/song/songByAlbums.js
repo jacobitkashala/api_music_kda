0c2818d3175bf1ff915d37d3b0ebcabb65e5acdf
jacobitkashala <jacobitkashala@gmail.com>
2022-05-05 18:24:03 -0400

    refactor:file songs by album

--- controllers/song/songByAlbums.js
+++ controllers/song/songByAlbums.js
@@ -4,54 +4,11 @@ const { Album, Songs } = require('../../models');
 
 const getSongGroupByAlbums = async (req, res) => {
   try {
-    // const SongGroupByAlbums = [];
-    // const SongInAlbum = {sequelize
-    //   title: '',
-    //   author: '',
-    //   songs: []
-    // };
-    // const Song = { , Users ,Songs
-    //   urlSong: '',
-    //   titleSongs: ''
-    // };
 
-    // console.log(Song);
-    // console.log(SongInAlbum);
-    // console.log(SongInAlbums);
-    // SELECT id_album,url_song,title_songs FROM Songs
-    //
-    // const titleAndSongs = await sequelize.query(
-    //   `SELECT id_album as idAlbum,url_song as urlSong,title_songs as titleSong
-    //     FROM Songs`,
-    //   {
-    //     type: QueryTypes.SELECT
-    //   }
-    // );
-    // const albumsJoinUsers = await sequelize.query(
-    //   `SELECT alb.id as idAlbum, us.name_user as author ,alb.title_album as titleAlbum,alb.url_image as urlImage, alb.is_top as isTop
-    //   FROM Albums as alb
-    //   JOIN Users as us ON us.id = alb.id_user`,
-    //   { type: QueryTypes.SELECT }
-    // );
-
-    // console.log(numberSonGroupByAlbum);
-    // const Songss = await Users.findAll({
-    //   attributes:["name_user"],
-    //   includes:{
-    //     model : Album,
-    //     attributes:["title_album", "is_top","url_image" ],
-    //     includes:{
-    //       model : Songs,
-    //       attributes:["id_songs", "url_song","title_songs" ],
-    //     }
-    //   }
-    // })
-
-    const Songss = await Album.findAll({
+    const songByAlbums = await Album.findAll({
       attributes: [
-        ['is_top', 'titleAlbum'],
         ['is_top', 'isTopAlbum'],
-        ['url_image', 'titleAlbum'],
+        ['url_image', 'urlImage'],
         ['title_album', 'titleAlbum']
       ],
       include: {
@@ -62,27 +19,8 @@ const getSongGroupByAlbums = async (req, res) => {
         ]
       }
     });
-    // console.log(Songss)
-    // albumsJoinUsers.forEach((item) => {
-    //   const song = songRefIdAlbums(item.idAlbum, titleAndSongs);
-    //   // console.log(song);
-    //   SongInAlbum.isTop = item.isTop;
-    //   SongInAlbum.author = item.author;
-    //   SongInAlbum.title = item.titleAlbum;
-    //   SongInAlbum.urlImage = item.urlImage;
-    //   SongInAlbum.songs.push(song);
 
-    //   SongGroupByAlbums.push(SongInAlbum);
-    // });
-    /**
- * SELECT alb.id as idAlbum, us.name_user as author ,alb.title_album as titleAlbum,alb.url_image as urlImage, alb.is_top as isTop FROM Albums as alb JOIN Users as us ON us.id = alb.id_user JOIN Songs as sg ON sg.id_album = alb.id
-
- */
-
-    // console.log(titleAndSongs);
-
-    //
-    return res.status(200).send(Songss);
+    return res.status(200).send(songByAlbums);
   } catch (error) {
     return res.status(500).send({ erreur: error });
   }
