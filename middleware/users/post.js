4ea78c8fc190b2ac472fa015ccc29fc58a67295f
jacobitkashala <jacobitkashala@gmail.com>
2022-05-12 15:58:19 -0400

    ddd

--- middleware/users/post.js
+++ middleware/users/post.js
@@ -4,15 +4,15 @@ const { body, validationResult } = require('express-validator');
 const usersgPostMiddleware = express();
 
 const validationDataSong = [
+  body('emailUser')
+  .notEmpty()
+  .isEmail()
+  .withMessage('emailUsers is empty')
+  .withMessage('emailUsers is not email'),
   body('nameUser').notEmpty().withMessage('nameUsers is empty'),
   body('sexUser').notEmpty().withMessage('nameUsers is empty'),
-  body('passwordUser').notEmpty().withMessage('passwordUsers is empty'),
   body('roleUser').notEmpty().withMessage('roleUsers is empty'),
-  body('emailUser')
-    .notEmpty()
-    .withMessage('emailUsers is empty')
-    .isEmail()
-    .withMessage('emailUsers is not email'),
+  body('passwordUser').notEmpty().withMessage('passwordUsers is empty'),
   body('numberPhoneUser')
     .notEmpty()
     .withMessage('numberPhoneUsers is empty')
