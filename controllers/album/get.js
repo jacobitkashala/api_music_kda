592980f7183d3925cff1fa6a2ef7995cedbbfcf6
jacobitkashala <jacobitkashala@gmail.com>
2022-05-18 15:27:43 -0400

    refactor:rout

--- controllers/album/get.js
+++ controllers/album/get.js
@@ -1,18 +1,23 @@
-const { Album } = require('../../models');
+const { Album,Users } = require('../../models');
 
 const getAlbum = async (req, res) => {
   try {
-    const albums = await Album.findAll({
+    const albums = await Users.findAll({
       attributes: [
-        ['id', 'id'],
-        ['is_top', 'isTop'],
-        ['id_album', 'idAlbum'],
-        ['url_image', 'urlImage'],
-        ['title_album', 'titleAlbum'],
-        ['contente_type', 'contenteType']
-      ]
+        ['name_user', 'nameUser'],
+      ],
+      include: {
+        model: Album,
+        attributes: [
+          ['id', 'id'],
+          ['is_top', 'isTop'],
+          ['id_album', 'idAlbum'],
+          ['url_image', 'urlImage'],
+          ['title_album', 'titleAlbum'],
+          ['contente_type', 'contenteType']
+        ]
+      }
     });
-
     return res.status(200).send(albums);
   } catch (error) {
     return res.status(500).send({ erreur: error });
