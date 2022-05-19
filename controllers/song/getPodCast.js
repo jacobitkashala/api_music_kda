f94401e00ffbc5db5d2c106bf4b4f4887cdc5a8f
jacobitkashala <jacobitkashala@gmail.com>
2022-05-19 18:10:45 -0400

    albumsbyPodcast

--- controllers/song/getPodCast.js
+++ controllers/song/getPodCast.js
@@ -1,13 +1,28 @@
-// const { Op } = require("sequelize");
-const { Songs } = require('../../models');
+const { Album, Songs } = require('../../models');
 
-const getSongs = async (req, res) => {
+const AlbumsPodCasts = async (req, res) => {
   try {
-    const songs = await Songs.findAll();
 
-    return res.status(200).send(songs);
+    const  podCastByAlbum= await Album.findAll({
+      attributes: [
+        ['is_top', 'isTopAlbum'],
+        ['url_image', 'urlImage'],
+        ['title_album', 'titleAlbum']
+      ],
+      include: {
+        model: Songs,
+        attributes: [
+          ['url_song', 'urlSong'],
+          ['title_songs', 'titleSong']
+        ]
+      }, where: {
+        contente_type: "Podcast"
+      }
+    });
+
+    return res.status(200).send(podCastByAlbum);
   } catch (error) {
     return res.status(500).send({ erreur: error });
   }
 };
-module.exports = getSongs;
+module.exports = AlbumsPodCasts;
