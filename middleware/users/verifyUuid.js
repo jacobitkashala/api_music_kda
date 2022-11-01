f9217ec13087451db06f5c42dbde4fa444e933a6
JacobitKashala <Jacobitkashala@gmail.com>
2022-11-01 15:25:32 -0400

    refactor:middleware son

--- middleware/users/verifyUuid.js
+++ middleware/users/verifyUuid.js
@@ -1,22 +1,18 @@
 const { Users } = require('../../models');
 
-const verifyUuid = async (req, res) => {
+const verifyUuid = async (req, res, next) => {
   const { uuid } = req.params;
   const dataResponse = {};
   try {
     const user = await Users.findOne({
       where: { id_user: uuid }
     });
-    // const user = await Users.destroy({
-    //   where: { id_user: uuid }
-    // });  next();
-    if (user == null) {
+    if (user === null) {
       dataResponse.error = true;
-      dataResponse.message = "Cet utilisateur n'existe pas ";
+      dataResponse.message = "Cet utilisateur n'existe pas";
       return res.status(400).send({ dataResponse });
     }
-
-    return res.status(200).send({ dataResponse });
+    next();
   } catch (error) {
     dataResponse.error = true;
     dataResponse.message = error;
